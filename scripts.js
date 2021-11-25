2c7afcfbd4d1c124805d33f44c88c4432f341a0d
Kevin-A-Stevens <kevin.alan.stevens@gmail.com>
2021-11-24 21:08:21 -0600

    Numbers, strings, and booleans

---
+++ scripts.js
@@ -0,0 +1,17 @@
+const monthlyRent = 500;
+const yearlyRent = monthlyRent * 12;
+console.log(yearlyRent);
+
+const myName = "Kevin";
+console.log(myName);
+
+const sentence = `Hello ${myName}`;
+console.log(sentence);
+
+const skyIsBlue = true;
+
+if (skyIsBlue) {
+  console.log("The sky is blue");
+} else {
+  console.log("The sky is not blue");
+}
