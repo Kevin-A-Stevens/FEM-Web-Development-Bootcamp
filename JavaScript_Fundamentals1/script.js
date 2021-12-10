c93a717e7b39e91cfb368e23286a3596c09c55a6
Kevin-A-Stevens <kevin.alan.stevens@gmail.com>
2021-12-09 19:41:17 -0600

    If-else statements

--- JavaScript_Fundamentals1/script.js
+++ JavaScript_Fundamentals1/script.js
@@ -91,3 +91,24 @@ console.log(`String
 with
 multiple
 lines`);
+
+// If / else statements
+
+const age = 19;
+const isOldEnough = age >= 18;
+if (isOldEnough) {
+  console.log("You are old enough to drive");
+} else {
+  console.log("You are not old enough to drive");
+}
+
+const birthYear = 1991;
+let century;
+
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+
+console.log(century);
